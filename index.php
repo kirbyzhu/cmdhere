3f528f433fa41b7ab7e9c3a977a804091f43be8b
phuslu <phuslu@hotmail.com>
2016-11-23 00:05:20 +0800

    add arm support to index.php
    Signed-off-by: phuslu <phuslu@hotmail.com>

--- index.php
+++ index.php
@@ -203,104 +203,116 @@ $sysInfo = sys_linux();
 
 function sys_linux()
 {
-    // CPU
-    if (false === ($str = @file("/proc/cpuinfo"))) return false;
-    $str = implode("", $str);
-    @preg_match_all("/model\s+name\s{0,}\:+\s{0,}([\w\s\)\(\@.-]+)([\r\n]+)/s", $str, $model);
-    @preg_match_all("/cpu\s+MHz\s{0,}\:+\s{0,}([\d\.]+)[\r\n]+/", $str, $mhz);
-    @preg_match_all("/cache\s+size\s{0,}\:+\s{0,}([\d\.]+\s{0,}[A-Z]+[\r\n]+)/", $str, $cache);
-    @preg_match_all("/bogomips\s{0,}\:+\s{0,}([\d\.]+)[\r\n]+/", $str, $bogomips);
-    if (false !== is_array($model[1]))
+  // CPU
+  if (false === ($str = @file("/proc/cpuinfo"))) return false;
+  $str = implode("", $str);
+  @preg_match_all("/processor\s{0,}\:+\s{0,}([\w\s\)\(\@.-]+)([\r\n]+)/s", $str, $processor);
+  @preg_match_all("/model\s+name\s{0,}\:+\s{0,}([\w\s\)\(\@.-]+)([\r\n]+)/s", $str, $model);
+  if (count($model[0]) == 0)
   {
-        $res['cpu']['num'] = sizeof($model[1]);
-    /*
-        for($i = 0; $i < $res['cpu']['num']; $i++)
-        {
-            $res['cpu']['model'][] = $model[1][$i].'&nbsp;('.$mhz[1][$i].')';
-            $res['cpu']['mhz'][] = $mhz[1][$i];
-            $res['cpu']['cache'][] = $cache[1][$i];
-            $res['cpu']['bogomips'][] = $bogomips[1][$i];
-        }*/
+    @preg_match_all("/Hardware\s{0,}\:+\s{0,}([\w\s\)\(\@.-]+)([\r\n]+)/s", $str, $model);
+  }
+  @preg_match_all("/cpu\s+MHz\s{0,}\:+\s{0,}([\d\.]+)[\r\n]+/", $str, $mhz);
+  if (count($mhz[0]) == 0)
+  {
+    $values = @file("/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq");
+    $mhz = array("", array(sprintf('%.3f', intval($values[0])/1000)));
+  }
+  @preg_match_all("/cache\s+size\s{0,}\:+\s{0,}([\d\.]+\s{0,}[A-Z]+[\r\n]+)/", $str, $cache);
+  @preg_match_all("/(?i)bogomips\s{0,}\:+\s{0,}([\d\.]+)[\r\n]+/", $str, $bogomips);
+  if (false !== is_array($model[1]))
+  {
+    $res['cpu']['num'] = sizeof($processor[1]);
     if($res['cpu']['num']==1)
       $x1 = '';
     else
       $x1 = ' ×'.$res['cpu']['num'];
     $mhz[1][0] = ' | 频率:'.$mhz[1][0];
-    $cache[1][0] = ' | 二级缓存:'.$cache[1][0];
+    if (count($cache[0]) > 0)
+      $cache[1][0] = ' | 二级缓存:'.$cache[1][0];
     $bogomips[1][0] = ' | Bogomips:'.$bogomips[1][0];
     $res['cpu']['model'][] = $model[1][0].$mhz[1][0].$cache[1][0].$bogomips[1][0].$x1;
-        if (false !== is_array($res['cpu']['model'])) $res['cpu']['model'] = implode("<br />", $res['cpu']['model']);
-        if (false !== is_array($res['cpu']['mhz'])) $res['cpu']['mhz'] = implode("<br />", $res['cpu']['mhz']);
-        if (false !== is_array($res['cpu']['cache'])) $res['cpu']['cache'] = implode("<br />", $res['cpu']['cache']);
-        if (false !== is_array($res['cpu']['bogomips'])) $res['cpu']['bogomips'] = implode("<br />", $res['cpu']['bogomips']);
+    if (false !== is_array($res['cpu']['model'])) $res['cpu']['model'] = implode("<br />", $res['cpu']['model']);
+    if (false !== is_array($res['cpu']['mhz'])) $res['cpu']['mhz'] = implode("<br />", $res['cpu']['mhz']);
+    if (false !== is_array($res['cpu']['cache'])) $res['cpu']['cache'] = implode("<br />", $res['cpu']['cache']);
+    if (false !== is_array($res['cpu']['bogomips'])) $res['cpu']['bogomips'] = implode("<br />", $res['cpu']['bogomips']);
   }
 
-    // NETWORK
+  // UPTIME
+  if (false === ($str = @file("/proc/uptime"))) return false;
+  $str = explode(" ", implode("", $str));
+  $str = trim($str[0]);
+  $min = $str / 60;
+  $hours = $min / 60;
+  $days = floor($hours / 24);
+  $hours = floor($hours - ($days * 24));
+  $min = floor($min - ($days * 60 * 24) - ($hours * 60));
+  if ($days !== 0) $res['uptime'] = $days."天";
+  if ($hours !== 0) $res['uptime'] .= $hours."小时";
+  $res['uptime'] .= $min."分钟";
 
-    // UPTIME
-    if (false === ($str = @file("/proc/uptime"))) return false;
-    $str = explode(" ", implode("", $str));
-    $str = trim($str[0]);
-    $min = $str / 60;
-    $hours = $min / 60;
-    $days = floor($hours / 24);
-    $hours = floor($hours - ($days * 24));
-    $min = floor($min - ($days * 60 * 24) - ($hours * 60));
-    if ($days !== 0) $res['uptime'] = $days."天";
-    if ($hours !== 0) $res['uptime'] .= $hours."小时";
-    $res['uptime'] .= $min."分钟";
-
-    // MEMORY
-    if (false === ($str = @file("/proc/meminfo"))) return false;
-    $str = implode("", $str);
-    preg_match_all("/MemTotal\s{0,}\:+\s{0,}([\d\.]+).+?MemFree\s{0,}\:+\s{0,}([\d\.]+).+?Cached\s{0,}\:+\s{0,}([\d\.]+).+?SwapTotal\s{0,}\:+\s{0,}([\d\.]+).+?SwapFree\s{0,}\:+\s{0,}([\d\.]+)/s", $str, $buf);
+  // MEMORY
+  if (false === ($str = @file("/proc/meminfo"))) return false;
+  $str = implode("", $str);
+  preg_match_all("/MemTotal\s{0,}\:+\s{0,}([\d\.]+).+?MemFree\s{0,}\:+\s{0,}([\d\.]+).+?Cached\s{0,}\:+\s{0,}([\d\.]+).+?SwapTotal\s{0,}\:+\s{0,}([\d\.]+).+?SwapFree\s{0,}\:+\s{0,}([\d\.]+)/s", $str, $buf);
   preg_match_all("/Buffers\s{0,}\:+\s{0,}([\d\.]+)/s", $str, $buffers);
 
-    $res['memTotal'] = round($buf[1][0]/1024, 2);
-    $res['memFree'] = round($buf[2][0]/1024, 2);
-    $res['memBuffers'] = round($buffers[1][0]/1024, 2);
+  $res['memTotal'] = round($buf[1][0]/1024, 2);
+  $res['memFree'] = round($buf[2][0]/1024, 2);
+  $res['memBuffers'] = round($buffers[1][0]/1024, 2);
   $res['memCached'] = round($buf[3][0]/1024, 2);
-    $res['memUsed'] = $res['memTotal']-$res['memFree'];
-    $res['memPercent'] = (floatval($res['memTotal'])!=0)?round($res['memUsed']/$res['memTotal']*100,2):0;
+  $res['memUsed'] = $res['memTotal']-$res['memFree'];
+  $res['memPercent'] = (floatval($res['memTotal'])!=0)?round($res['memUsed']/$res['memTotal']*100,2):0;
 
-    $res['memRealUsed'] = $res['memTotal'] - $res['memFree'] - $res['memCached'] - $res['memBuffers']; //真实内存使用
+  $res['memRealUsed'] = $res['memTotal'] - $res['memFree'] - $res['memCached'] - $res['memBuffers']; //真实内存使用
   $res['memRealFree'] = $res['memTotal'] - $res['memRealUsed']; //真实空闲
-    $res['memRealPercent'] = (floatval($res['memTotal'])!=0)?round($res['memRealUsed']/$res['memTotal']*100,2):0; //真实内存使用率
+  $res['memRealPercent'] = (floatval($res['memTotal'])!=0)?round($res['memRealUsed']/$res['memTotal']*100,2):0; //真实内存使用率
 
   $res['memCachedPercent'] = (floatval($res['memCached'])!=0)?round($res['memCached']/$res['memTotal']*100,2):0; //Cached内存使用率
 
-    $res['swapTotal'] = round($buf[4][0]/1024, 2);
-    $res['swapFree'] = round($buf[5][0]/1024, 2);
-    $res['swapUsed'] = round($res['swapTotal']-$res['swapFree'], 2);
-    $res['swapPercent'] = (floatval($res['swapTotal'])!=0)?round($res['swapUsed']/$res['swapTotal']*100,2):0;
+  $res['swapTotal'] = round($buf[4][0]/1024, 2);
+  $res['swapFree'] = round($buf[5][0]/1024, 2);
+  $res['swapUsed'] = round($res['swapTotal']-$res['swapFree'], 2);
+  $res['swapPercent'] = (floatval($res['swapTotal'])!=0)?round($res['swapUsed']/$res['swapTotal']*100,2):0;
 
-    // LOAD Board
-    if (glob("/sys/devices/virtual/dmi/id/board_name")) {
-        $res['boardVendor'] = file('/sys/devices/virtual/dmi/id/board_vendor')[0];
-        $res['boardName'] = file('/sys/devices/virtual/dmi/id/board_name')[0];
-        $res['boardVersion'] = file('/sys/devices/virtual/dmi/id/board_version')[0];
-    }
+  // LOAD Board
+  if (glob("/sys/devices/virtual/dmi/id/board_name")) {
+      $res['boardVendor'] = file('/sys/devices/virtual/dmi/id/board_vendor')[0];
+      $res['boardName'] = file('/sys/devices/virtual/dmi/id/board_name')[0];
+      $res['boardVersion'] = file('/sys/devices/virtual/dmi/id/board_version')[0];
+  } else if (glob("/sys/devices/virtual/android_usb/android0/f_rndis/manufacturer")) {
+      $res['boardVendor'] = file('/sys/devices/virtual/android_usb/android0/f_rndis/manufacturer')[0];
+      $res['boardName'] = '';
+      $res['boardVersion'] = '';
+  }
 
-    // LOAD BIOS
-    if (glob("/sys/devices/virtual/dmi/id/bios_vendor")) {
-        $res['BIOSVendor'] = file('/sys/devices/virtual/dmi/id/bios_vendor')[0];
-        $res['BIOSVersion'] = file('/sys/devices/virtual/dmi/id/bios_version')[0];
-        $res['BIOSDate'] = file('/sys/devices/virtual/dmi/id/bios_date')[0];
-    }
+  // LOAD BIOS
+  if (glob("/sys/devices/virtual/dmi/id/bios_vendor")) {
+      $res['BIOSVendor'] = file('/sys/devices/virtual/dmi/id/bios_vendor')[0];
+      $res['BIOSVersion'] = file('/sys/devices/virtual/dmi/id/bios_version')[0];
+      $res['BIOSDate'] = file('/sys/devices/virtual/dmi/id/bios_date')[0];
+  } else if (glob("/sys/devices/virtual/android_usb/android0/iProduct")) {
+      $res['BIOSVendor'] = file('/sys/devices/virtual/android_usb/android0/iManufacturer')[0];
+      $res['BIOSVersion'] = file('/sys/devices/virtual/android_usb/android0/iProduct')[0];
+      $res['BIOSDate'] = '';
+  }
 
-    // LOAD DISK
-    if (glob("/sys/class/block/s*/device/model")) {
-        $res['diskModel'] = file(glob("/sys/class/block/s*/device/model")[0])[0];
-        $res['diskVendor'] = file(glob("/sys/class/block/s*/device/vendor")[0])[0];
-    }
+  // LOAD DISK
+  if (glob("/sys/class/block/s*/device/model")) {
+      $res['diskModel'] = file(glob("/sys/class/block/s*/device/model")[0])[0];
+      $res['diskVendor'] = file(glob("/sys/class/block/s*/device/vendor")[0])[0];
+  } else if (glob("/sys/class/block/mmc*/device/type")) {
+      $res['diskModel'] = file(glob("/sys/class/block/mmc*/device/name")[0])[0];
+      $res['diskVendor'] = file(glob("/sys/class/block/mmc*/device/type")[0])[0];
+  }
 
-    // LOAD AVG
-    if (false === ($str = @file("/proc/loadavg"))) return false;
-    $str = explode(" ", implode("", $str));
-    $str = array_chunk($str, 4);
-    $res['loadAvg'] = implode(" ", $str[0]);
+  // LOAD AVG
+  if (false === ($str = @file("/proc/loadavg"))) return false;
+  $str = explode(" ", implode("", $str));
+  $str = array_chunk($str, 4);
+  $res['loadAvg'] = implode(" ", $str[0]);
 
-    return $res;
+  return $res;
 }
 
 //比例条
