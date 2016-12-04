99c187adee8576ff0d4c46d079c996f14a17c497
phuslu <phuslu@hotmail.com>
2016-12-04 11:25:45 +0800

    add rdpproxy.go

---
+++ rdpproxy.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"net"
+	"os/exec"
+
+	"github.com/phuslu/glog"
+)
+
+func forward(lconn net.Conn, raddr string) {
+	if err := exec.Command("wakeonlan", "18:66:DA:17:A2:95").Run(); err != nil {
+		glog.Warningf("exec wakeonlan error: %+v", err)
+	}
+
+	glog.Infof("try connect to %+v for %+v forwarding", raddr, lconn.RemoteAddr())
+	rconn, err := net.Dial("tcp", raddr)
+	if err != nil {
+		glog.Errorf("net.Dial(%#v) error: %v", raddr, err)
+		return
+	}
+
+	glog.Infof("forward %+v to %+v", lconn.RemoteAddr(), rconn.RemoteAddr())
+	go func() {
+		defer rconn.Close()
+		defer lconn.Close()
+		io.Copy(rconn, lconn)
+	}()
+	go func() {
+		defer lconn.Close()
+		defer rconn.Close()
+		io.Copy(lconn, rconn)
+	}()
+}
+
+func main() {
+	var laddr, raddr string
+	flag.StringVar(&laddr, "laddr", "0.0.0.0:13389", "local address")
+	flag.StringVar(&raddr, "raddr", "192.168.1.3:3389", "remote address")
+
+	flag.Parse()
+
+	ln, err := net.Listen("tcp", laddr)
+	if err != nil {
+		glog.Fatalf("net.Listen(%+v) error: %+v", laddr, err)
+		return
+	}
+	defer ln.Close()
+
+	glog.Infof("Serving on %+v", ln.Addr())
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			glog.Fatalf("%+v Accept error: %+v", ln.Addr(), err)
+		}
+		go forward(conn, raddr)
+	}
+}
+
