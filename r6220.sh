8f08eff1db8adfe422d44ba7954a20877939dd3b
Phus Lu <phuslu@hotmail.com>
2017-03-25 21:25:30 +0800

    update r6220.sh
    Signed-off-by: Phus Lu <phuslu@hotmail.com>

--- r6220.sh
+++ r6220.sh
@@ -3,7 +3,7 @@
 #    http://192.168.1.1/setup.cgi?todo=debug
 # 2. add below xshell logon script 
 #    test -f /tmp/r6220.sh && exec /tmp/r6220.sh || wget http://phuslu.github.io/mips/r6220.sh -O /tmp/r6220.sh && chmod +x /tmp/r6220.sh
-if test -x /tmp/root/.local/bin/busybox; then
+if test -x /tmp/root/bin/sh; then
     export TERM=xterm
     export USER=root
     export HOME=/tmp/root
@@ -12,25 +12,25 @@ if test -x /tmp/root/.local/bin/busybox; then
     export HISTSIZE=100000
     export HISTFILESIZE=2000000
     export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
-    export PATH=$HOME/.local/bin:$PATH
+    export PATH=$HOME/bin:$PATH
     cd $HOME
-    if test -x $HOME/.local/bin/bash; then
+    if test -x $HOME/bin/bash; then
         export PS1='\[\e]0;\h:\w\a\]\n\[\e[01;32m\]\u@\h\[\e[00;33m\] \w\n\[\e[1;$((31+3*!$?))m\]\$\[\e[00m\] '
-        exec $HOME/.local/bin/bash --login
+        exec $HOME/bin/bash --login
     else
         export PS1='\[\e[01;32m\]\u@\h\[\e[00;33m\] \w \[\e[1;$((31+3*!$?))m\]\$\[\e[00m\] '
-        exec $HOME/.local/bin/sh --login
+        exec $HOME/bin/sh --login
     fi
 else
     export HOME=/tmp/root
-    mkdir -p $HOME/.local/bin
-    cd $HOME/.local/bin
+    mkdir -p $HOME/bin
+    cd $HOME/bin
     wget http://phuslu.github.io/mips/busybox-mipsel -O busybox
     wget http://phuslu.github.io/mips/bash-mipsel -O bash
     wget http://files.lancethepants.com/Binaries/htop/mipsel/htop%201.0.3/htop
     wget http://files.lancethepants.com/Binaries/curl/mipsel/curl%207.40.0%20%28MIPSR1%29/curl
+    wget http://phuslu.github.io/bashrc -O $HOME/.bash_login
     chmod +x busybox bash htop curl
-    $HOME/.local/bin/busybox --install -s .
-    wget http://phuslu.github.io/.bashrc -O $HOME/.bash_login
+    $HOME/bin/busybox --install -s .
 fi
 
